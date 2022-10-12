09bc5b27fae44adffa5d149a06fdb442f33cee98
BLIZERTO <74712916+BLIZERTO@users.noreply.github.com>
2022-10-12 20:47:58 +0200

    Creation Formulaire Annonces encore

--- src/src/Controller/AnnoncesController.php
+++ src/src/Controller/AnnoncesController.php
@@ -3,8 +3,12 @@
 namespace App\Controller;
 
 use App\Entity\Annonces;
+use App\Form\CreationAnnonceType;
 use App\Repository\AnnoncesRepository;
+use Doctrine\DBAL\Exception\DatabaseDoesNotExist;
+use Doctrine\ORM\EntityManagerInterface;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 
@@ -19,16 +23,21 @@ class AnnoncesController extends AbstractController {
     }
 
     #[Route('/creation', name: 'app_creation', methods: ["GET", "POST"])]
-    public function creationAnnonces(AnnoncesRepository $annoncesRepository): Response {
-        $form = $this->createForm(CreationAnnonceType::class);
-       return $this->render('annonces/creationAnnonce.html.twig', [
-           'creationAnnonce' => $form->createView(),
-       ]);
+    public function creationAnnonces(EntityManagerInterface $em, Request $request): Response {
+       $form = $this->createForm(CreationAnnonceType::class);
+        $form->handleRequest($request);
+        if ($form->isSubmitted() && $form->isValid()) {
+            $data = $form->getData();
+            $em->persist($data);
+            $em->flush();
+        }
+        return $this->render('annonces/creationAnnonce.html.twig', [
+            'creationAnnonce' => $form->createView(),
+        ]);
     }
 
     #[Route('/delete/{annonce_id}', name: 'app_delete')]
-    public function deleteAnnonces(AnnoncesRepository $annoncesRepository, int $annonce_id): Response
-    {
+    public function deleteAnnonces(AnnoncesRepository $annoncesRepository, int $annonce_id): Response {
         $annonce = $annoncesRepository->find($annonce_id);
         $annoncesRepository->remove($annonce);
         return $this->redirectToRoute('app_accueil');
