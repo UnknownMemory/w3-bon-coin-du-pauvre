801fc51456a4fa7dde9b1dd4068342b6dc2eb4c1
BLIZERTO <74712916+BLIZERTO@users.noreply.github.com>
2022-10-22 17:56:59 +0200

    ajout commentaire2

---
+++ src/src/Form/CommentaireType.php
@@ -0,0 +1,26 @@
+<?php
+
+namespace App\Form;
+
+use App\Entity\Commentaires;
+use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\Extension\Core\Type\TextType;
+use Symfony\Component\Form\FormBuilderInterface;
+use Symfony\Component\OptionsResolver\OptionsResolver;
+
+class CommentaireType extends AbstractType
+{
+    public function buildForm(FormBuilderInterface $builder, array $options): void
+    {
+        $builder
+            ->add('message', TextType::class)
+        ;
+    }
+
+    public function configureOptions(OptionsResolver $resolver): void
+    {
+        $resolver->setDefaults([
+            'data_class' => Commentaires::class,
+        ]);
+    }
+}
