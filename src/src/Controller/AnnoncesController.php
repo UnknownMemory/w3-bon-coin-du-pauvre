e16d0e3a0c9b87858aa2d2c7edab3fa1c560a284
Thomas-Torrente <thomas.torrente@gmail.com>
2022-10-26 16:17:38 +0200

    update des routes

--- src/src/Controller/AnnoncesController.php
+++ src/src/Controller/AnnoncesController.php
@@ -3,20 +3,16 @@
 namespace App\Controller;
 
 use App\Entity\Annonce;
-
-use App\Entity\Images;
-
-use App\Form\CreationAnnonceType;
 use App\Service\SlugService;
+use App\Form\CreationAnnonceType;
 use App\Service\UploadImageService;
 use App\Repository\AnnonceRepository;
-use Cocur\Slugify\Slugify;
 use Doctrine\ORM\EntityManagerInterface;
-use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\Security\Core\Security;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
-use Symfony\Component\Security\Core\Security;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 
 #[Route('/annonces')]
 class AnnoncesController extends AbstractController
@@ -36,14 +32,38 @@ class AnnoncesController extends AbstractController
         ]);
     }
 
-    #[Route('/{slug}', name: 'app_oneannonce')]
-    public function oneAnnonce(Annonce $annonce): Response
+
+
+
+
+    #[Route('/creation', name: 'app_creation', methods: ["GET", "POST"])]
+    public function creationAnnonces(AnnonceRepository $annonceRepository, Request $request, SlugService $slugService, UploadImageService $upload): Response
     {
-        return $this->render('annonces/oneAnnonce.html.twig', [
-            'oneAnnonce' => $annonce,
+        if (!$this->getUser()) {
+            return $this->redirectToRoute('app_all');
+        }
+        $annonce = new Annonce();
+        $form = $this->createForm(CreationAnnonceType::class, $annonce);
+        $form->handleRequest($request);
+        if ($form->isSubmitted() && $form->isValid()) {
+
+
+
+            $images = $form['images']->getData();
+            /* On utilise notre service pour upload l'image et on lui passe en parametre les données de nos images, le nom du site, et l'endroit ou l'on veut upload les images*/
+            $arrayImage = $upload->upload($images, "-le-bon-coin-du-pauvre", '/public/assets/img/upload');
+            /* On utilise la méthode setImage pour enregistrer les noms des images dans BDD (ici sous forme de tableau) */
+            $annonce->setImages($arrayImage);
+
+            $annonce->setVendeur($this->getUser());
+            $annonce->setDate(new \DateTime());
+            $annonce->setSlug($slugService->getSlug($annonce));
+            $annonceRepository->save($annonce, true);
+        }
+        return $this->render('annonces/creationAnnonce.html.twig', [
+            'creationAnnonce' => $form->createView(),
         ]);
     }
-
 
 
     #[Route('/modification/{annonce_id}', name: 'app_modify', methods: ["GET", "POST"])]
@@ -65,39 +85,16 @@ class AnnoncesController extends AbstractController
 
             return $this->renderForm('annonces/modification.html.twig', [
                 'annonceForm' => $form,
-                'nomAnnonce' => $annonce->getTitre(),
+                'nameAnnonce' => $annonce->getTitre(),
             ]);
         }
     }
 
-
-    #[Route('/creation', name: 'app_creation', methods: ["GET", "POST"])]
-    public function creationAnnonces(AnnonceRepository $annonceRepository, Request $request, SlugService $slugService, UploadImageService $upload): Response
+    #[Route('/{slug}', name: 'app_oneannonce')]
+    public function oneAnnonce(Annonce $annonce): Response
     {
-        if (!$this->getUser()) {
-            return $this->redirectToRoute('app_all');
-        }
-        $annonce = new Annonce();
-        $form = $this->createForm(CreationAnnonceType::class, $annonce);
-        $form->handleRequest($request);
-        if ($form->isSubmitted() && $form->isValid()) {
-            $images = $form['images']->getData();
-
-            /* Gestion des images */
-            /* On récupérer les différentes images */
-            $images = $form['images']->getData();
-            /* On utilise notre service pour upload l'image et on lui passe en parametre les données de nos images, le nom du site, et l'endroit ou l'on veut upload les images*/
-            $arrayImage = $upload->upload($images, "-le-bon-coin-du-pauvre", '/public/assets/img/upload');
-            /* On utilise la méthode setImage pour enregistrer les noms des images dans BDD (ici sous forme de tableau) */
-            $annonce->setImages($arrayImage);
-
-            $annonce->setVendeur($this->getUser());
-            $annonce->setDate(new \DateTime());
-            $annonce->setSlug($slugService->getSlug($annonce));
-            $annonceRepository->save($annonce, true);
-        }
-        return $this->render('annonces/creationAnnonce.html.twig', [
-            'creationAnnonce' => $form->createView(),
+        return $this->render('annonces/oneAnnonce.html.twig', [
+            'oneAnnonce' => $annonce,
         ]);
     }
 
